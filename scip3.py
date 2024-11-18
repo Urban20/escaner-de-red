a82d9a2e750785860921ff61e34099b20667ec1b
Matias <matiasurbaneja@hotmail.com.ar>
2024-11-18 01:12:29 -0300

    agregado de excepcion keyboard interrupt

--- scip3.py
+++ scip3.py
@@ -43,141 +43,144 @@ def crear_crawler(ip):
     crawler.scrapping_shodan()
     crawler.obtener_links()
 
+try:
+    if params.param.masivo:
+        puertos = list(range(1,65535))
+    else:
+        puertos = data.puertos
+        
 
-if params.param.masivo:
-    puertos = list(range(1,65535))
-else:
-    puertos = data.puertos
-    
+
+    #acciones de los parametros-----------------
+    if params.param.shodan:
+        try:
+            if params.param.ip != None:
+                if ',' in params.param.ip:
+                    for x in params.param.ip.split(','):
+                        crear_crawler(x)   
+                else:       
+                    crear_crawler(params.param.ip)
+            else:
+                print(Fore.RED+'especificar parametro [-ip]')
+        except Exception as e:
+            print(f'''error al buscar en shodan:
+    {e}
+    ''')
 
 
-#acciones de los parametros-----------------
-if params.param.shodan:
-    try:
+    if params.param.agresivo:
+        try:
+            if params.param.ip != None:
+                print(Fore.WHITE+'''\n
+    #################################################''')
+                print(Fore.WHITE+'escaneo agresivo en curso...')
+
+                for x in puertos:
+                    hilo = threading.Thread(target=func.scan_agresivo,args=(params.param.ip,x))
+                    hilo.start()
+                if data.p_abiertos:
+
+                    print(Fore.WHITE+func.dataf())
+                    if params.param.info:
+                        for x in data.p_abiertos:
+                            func.informacion(params.param.ip,x)
+
+                    func.preg_informe(ip=params.param.ip,lista=data.p_abiertos)
+                else:
+                    print(Fore.RED+'no se encontro ningun puerto')
+                
+
+            else:
+                print(Fore.RED+'especificar parametro [-ip]')        
+        except Exception as e:
+            print(Fore.RED+f'''error al escanear con metodo agresivo:
+    {e}''')
+        finally:
+            data.p_abiertos.clear()
+            
+    elif params.param.normal and params.param.buscar == None:
         if params.param.ip != None:
-            if ',' in params.param.ip:
-                for x in params.param.ip.split(','):
-                    crear_crawler(x)   
-            else:       
-                crear_crawler(params.param.ip)
+
+            scan= inicio_scan(msg='escaneo normal en curso...')
+            threading.Thread(target=func.progreso).start()
+            func.scan_normal(params.param.ip,scan)   
+            
+        else:
+            print(Fore.RED+'especificar parametro [-ip]') 
+
+    elif params.param.selectivo:
+        if params.param.ip != None:
+
+            scan= inicio_scan(msg='escaneo selectivo en curso...')
+
+            func.scan_selectivo(params.param.ip,scan,params.param.selectivo)
+            if params.param.info:
+                for x in data.p_abiertos:
+                    func.informacion(params.param.ip,x)
+
         else:
             print(Fore.RED+'especificar parametro [-ip]')
-    except Exception as e:
-        print(f'''error al buscar en shodan:
-{e}
-''')
-
-
-if params.param.agresivo:
-    try:
-        if params.param.ip != None:
-            print(Fore.WHITE+'''\n
-#################################################''')
-            print(Fore.WHITE+'escaneo agresivo en curso...')
-
-            for x in puertos:
-                hilo = threading.Thread(target=func.scan_agresivo,args=(params.param.ip,x))
-                hilo.start()
-            if data.p_abiertos:
-
-                print(Fore.WHITE+func.dataf())
-                if params.param.info:
-                    for x in data.p_abiertos:
-                        func.informacion(params.param.ip,x)
-
-                func.preg_informe(ip=params.param.ip,lista=data.p_abiertos)
+    elif params.param.ip != None and params.param.descubrir:
+        try:
+        
+            if params.param.timeout != None:
+                timeout_ = params.param.timeout
             else:
-                print(Fore.RED+'no se encontro ningun puerto')
+                timeout_ = 4
+        
+            print(Fore.GREEN+'rastreando ips privadas: ')
+            for x in range(1,255):
+                ejec= threading.Thread(target=func.descubrir_red,args=(params.param.ip,x,timeout_))
+                if 'x' in params.param.ip:
+                    ejec.start()
+                else:
+                    print(Fore.RED+'la ip debe contener una x al final, ejemplo "192.168.0.x"')
+                    break
+            ejec.join()
             
-
-        else:
-            print(Fore.RED+'especificar parametro [-ip]')        
-    except Exception as e:
-        print(Fore.RED+f'''error al escanear con metodo agresivo:
-{e}''')
-    finally:
-        data.p_abiertos.clear()
-        
-elif params.param.normal and params.param.buscar == None:
-    if params.param.ip != None:
-
-        scan= inicio_scan(msg='escaneo normal en curso...')
-        threading.Thread(target=func.progreso).start()
-        func.scan_normal(params.param.ip,scan)   
-        
-    else:
-        print(Fore.RED+'especificar parametro [-ip]') 
-
-elif params.param.selectivo:
-    if params.param.ip != None:
-
-        scan= inicio_scan(msg='escaneo selectivo en curso...')
-
-        func.scan_selectivo(params.param.ip,scan,params.param.selectivo)
-        if params.param.info:
-            for x in data.p_abiertos:
-                func.informacion(params.param.ip,x)
-
-    else:
-        print(Fore.RED+'especificar parametro [-ip]')
-elif params.param.ip != None and params.param.descubrir:
-    try:
-    
-        if params.param.timeout != None:
-            timeout_ = params.param.timeout
-        else:
-            timeout_ = 4
-    
-        print(Fore.GREEN+'rastreando ips privadas: ')
-        for x in range(1,255):
-            ejec= threading.Thread(target=func.descubrir_red,args=(params.param.ip,x,timeout_))
-            if 'x' in params.param.ip:
-                ejec.start()
-            else:
-                print(Fore.RED+'la ip debe contener una x al final, ejemplo "192.168.0.x"')
-                break
-        ejec.join()
-        
-        for ip in func.ipv4:
-        
-            ipv4 = objs.Ipv4(ip=ip)
-            codigo = ipv4.ttl()
-            nombre = ipv4.obtener_nombre()
+            for ip in func.ipv4:
             
-            if codigo != None:
-                print(Fore.GREEN+f'\n{ip}:\n')
-                print(data.data_ttl[codigo])
-                print(Fore.CYAN+f'nombre del dispositivo: {nombre}')
+                ipv4 = objs.Ipv4(ip=ip)
+                codigo = ipv4.ttl()
+                nombre = ipv4.obtener_nombre()
+                
+                if codigo != None:
+                    print(Fore.GREEN+f'\n{ip}:\n')
+                    print(data.data_ttl[codigo])
+                    print(Fore.CYAN+f'nombre del dispositivo: {nombre}')
+                
+        except:
+            pass
+    if params.param.buscar != None and not params.param.normal:
+        try:
             
-    except:
-        pass
-if params.param.buscar != None and not params.param.normal:
-    try:
-        
-        while func.n < params.param.buscar:
+            while func.n < params.param.buscar:
+                
+                busq = func.buscar()
+                data.elementos.clear()
+                if busq != None:
+                    print(busq)
+                    func.n+=1
             
-            busq = func.buscar()
-            data.elementos.clear()
-            if busq != None:
-                print(busq)
-                func.n+=1
-        
-    except Exception as e:
-        print(f'''ocurrio un error:
-{e}''')
+        except Exception as e:
+            print(f'''ocurrio un error:
+    {e}''')
 
-if params.param.ayuda:
-    func.ayuda()
+    if params.param.ayuda:
+        func.ayuda()
 
-if params.param.borrar:
-    func.borrar_arch()
+    if params.param.borrar:
+        func.borrar_arch()
 
-elif params.param.abrir:
-    func.abrir_arch()
+    elif params.param.abrir:
+        func.abrir_arch()
 
-if params.param.lectura:
-    try:
-        with open(data.nombre_arch,'r') as registro:
-            print(Fore.CYAN+registro.read())
-    except FileNotFoundError:
-        print(Fore.RED+'registro no encontrado')       
+    if params.param.lectura:
+        try:
+            with open(data.nombre_arch,'r') as registro:
+                print(Fore.CYAN+registro.read())
+        except FileNotFoundError:
+            print(Fore.RED+'registro no encontrado')       
+except KeyboardInterrupt:
+    func.deten = True
+    exit()
